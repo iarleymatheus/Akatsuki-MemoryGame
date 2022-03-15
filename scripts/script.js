e02dbe019e0b3751048ad9f1100cd9dfbd3b4290
iarleymatheus <yarley.matheus@hotmail.com>
2022-03-15 17:53:59 -0300

    embaralhamento adicionado

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
-const FRONT  = 'card_front'
-const BACK = 'card_back'
+const FRONT  = 'card_front';
+const BACK = 'card_back';
+const CARD = 'card';
+const ICON = 'icon';
+
 
 let persons = [
    'deidara',
@@ -13,15 +16,82 @@ let persons = [
    'tobi',
    'zetsu'];
   
+   let cards = null;
+   startGame();
+  
+   function startGame(){
+       cards = createCardsFromPerson(persons);
+       shuffleCards(cards);
+       
+       
+       initializeCards(cards);
+       
+   }
+ function initializeCards(cards){
+  let gameBoard = document.getElementById("gameBord")
+  cards.forEach((card)=>{
+      let cardElement = document.createElement('div');
+      cardElement.id = card.id;
+      cardElement.classList.add(CARD);
+      cardElement.dataset.icon = card.icon;
+       createCardContent(card, cardElement);
+
+      cardElement.addEventListener('click', flipCard);
+      gameBoard.appendChild(cardElement);
+
+
+
+
+  })
+  
+
+ }
+
+   function createCardContent(card , cardElement){
+       createCardFace(FRONT , card , cardElement);
+       createCardFace(BACK , card , cardElement);
+
+     }
+     function createCardFace(face , card, element){
+         let cardElementFace = document.createElement('div')
+         cardElementFace.classList.add(face)
+         if(face === FRONT){
+             let iconElement = document.createElement('img');
+             iconElement.classList.add(ICON);
+             iconElement.src = "./assets/" + card.icon + ".png";
+             cardElementFace.appendChild(iconElement);
+
+         }else{
+             let iconElementBack = document.createElement('img');
+             iconElementBack.classList.add(ICON);
+             iconElementBack.src = "./assets/akat.jpg";
+             cardElementFace.appendChild(iconElementBack)
+
+         }
+         element.appendChild(cardElementFace);
+     }
+
+
+   function shuffleCards(cards){
+   let currentIndex = cards.length;
+   let radomIndex = 0;
+    while(currentIndex !==0){
+        radomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex --;
+        [cards[radomIndex], cards[currentIndex]] = [cards[currentIndex], cards[radomIndex]]
+    }
+
+
+   }
    createCardsFromPerson(persons);
 
    function createCardsFromPerson(persons){
        let cards = [];
 
-       for (let perso of persons){
+       persons.forEach((perso)=> {
            cards.push(createPairFromPersons(perso));
-       }
        
+       })
        return cards.flatMap(pair => pair);
    }
 
@@ -43,4 +113,7 @@ let persons = [
    
     function createIdFromPersons(perso){
         return perso + parseInt (Math.random() * 1000)
+    }
+    function flipCard(){
+
     }
